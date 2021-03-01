b06cc5136037a4d475a795be13c884114514a704
Mariusz Wieczorek <m.wieczorek1972@gmail.com>
2021-03-01 15:01:20 +0100

    x

---
+++ Core/ViewModels/AdvertViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAdvert.Core.ViewModels
+{
+    public class AdvertViewModel
+    {
+        public IEnumerable<Advert> Adverts { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public FilterAdverts FilterAdverts { get; set; }
+
+    }
+}
